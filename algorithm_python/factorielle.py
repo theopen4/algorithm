29a3837ff233e08e3ec6a3f009bfa2f1d49b3934
theopen4 <kielze86@gmail.com>
2023-05-11 19:04:27 +0100

    recursion facorielle

---
+++ algorithm_python/factorielle.py
@@ -0,0 +1,16 @@
+def factorielle(nb: int ) -> int:
+    if nb == 0:
+        return 1
+    for d in range(1,  nb):
+        nb = d * nb
+
+    return nb
+
+print(factorielle(6))
+def recur_factorielle(nb):
+   if nb == 0 or nb == 1:
+       return 1
+   return nb * recur_factorielle(nb - 1)
+
+print(recur_factorielle(6))  
+ 
