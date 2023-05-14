d5565271dc1fed62316ec2d18f1c725161e9c6a0
theopen4 <kielze86@gmail.com>
2023-05-14 17:46:10 +0100

    spy number intervalle

--- algorithm_python/spy_number.py
+++ algorithm_python/spy_number.py
@@ -6,4 +6,15 @@ def spy_number(nb: int) -> bool:
     return sum_n == produ_nb
 print(spy_number(123))  
 
+def spy_number_intervalle(inf: int or float, sup: int or float) -> list:
+    list_spy_number: list = []
+    for d in range(inf, sup):
+        if spy_number(d):
+            list_spy_number.append(d)
+
+    return list_spy_number    
+
+
+print(spy_number_intervalle(1,100))
+
     
