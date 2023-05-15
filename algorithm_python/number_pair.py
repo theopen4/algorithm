d2971c7f8fd9317fb01cd5dec0d51dc762a0d3a8
theopen4 <kielze86@gmail.com>
2023-05-15 13:46:55 +0100

    pair number and list numberpair

---
+++ algorithm_python/number_pair.py
@@ -0,0 +1,14 @@
+def pair(n: int or float) -> bool:
+    return n % 2 == 0
+
+print(pair(3))
+
+def intervalle_pair(inf: int or float, sup: int or float)->list[int]:
+    number_pair = []
+    for d in range(inf , sup):
+        if pair(d):
+            number_pair.append(d)
+
+    return number_pair
+
+print(intervalle_pair(1,100))
