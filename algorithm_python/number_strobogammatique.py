bccf9ad2e1b7374e8393dce3d275c125372bd423
theopen4 <kielze86@gmail.com>
2023-05-16 13:32:07 +0100

    strogramatique

--- algorithm_python/number_strobogammatique.py
+++ algorithm_python/number_strobogammatique.py
@@ -16,3 +16,21 @@ def number_strobogrammatique(n: int or float)-> bool:
 print(number_strobogrammatique(16885))
 #second methode 
 
+def number_strobogrammatique2(n: int or float) -> bool:
+    strobo = {"0": "0", "1": "1", "6": "6", "8": "8", "9": "9"}
+    if type(n) is int:
+        list(str(n))
+
+    left = 0
+    rigth = len(n -1)
+    while rigth - left >= 0:
+        if not n[left] in strobo.keys or n[left] in strobo.keys or n[left] !=strobo[n[rigth]]:
+            return False
+        rigth -= 1
+        left += 1
+
+    return True
+    
+
+for i in range(1,1000):
+    print("{} : {}".format(i, number_strobogrammatique(i)))
